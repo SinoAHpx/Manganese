923fa16f497f55428e6cd93126dacfa69f4a7922
AHpx <AHpx@yandex.com>
2022-01-20 17:12:36 +0800

    Added ProcessManipulator.cs

---
+++ Manganese/Process/ProcessManipulator.cs
@@ -0,0 +1,16 @@
+﻿namespace Manganese.Process;
+
+using System.Diagnostics;
+
+public static class ProcessManipulator
+{
+    public static string? ReadOutputLine(this Process process)
+    {
+        return process.StandardOutput.ReadLine();
+    }
+
+    public static async Task<string?> ReadOutputLineAsync(this Process process)
+    {
+        return await process.StandardOutput.ReadLineAsync();
+    }
+}
