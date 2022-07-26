743d5b644ad93aa80ee41df3b49ad5c8d16fa671
AHpx <ahpx@yandex.com>
2022-07-26 22:50:48 +0800

    Added data validation

---
+++ Manganese.UnitTest/Data/CommonDataDetectorShould.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Manganese.Data;
+using Xunit;
+
+namespace Manganese.UnitTest.Data;
+
+public class CommonDataDetectorShould
+{
+    [Fact]
+    public void ThrowIfNul()
+    {
+        string? a = null;
+        Assert.Throws<InvalidOperationException>(() => a.ThrowIfNull(new InvalidOperationException()));
+
+        Assert.Equal("awd", "awd".ThrowIfNull());
+        Assert.Equal(114514, 114514.ThrowIfNull());
+    }
+}
---
+++ Manganese/Data/CommonDataDetector.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Manganese.Data;
+
+/// <summary>
+/// General detector
+/// </summary>
+public static class CommonDataDetector
+{
+    /// <summary>
+    /// Throw an exception if object is null 
+    /// </summary>
+    /// <param name="t"></param>
+    /// <param name="exception"></param>
+    /// <typeparam name="T">Target object</typeparam>
+    /// <returns>Object itself</returns>
+    public static T ThrowIfNull<T>(this T? t, Exception? exception = null)
+    {
+        if (t == null)
+            throw exception ?? new InvalidOperationException("Object cannot be null");
+
+        return t;
+    }
+}
