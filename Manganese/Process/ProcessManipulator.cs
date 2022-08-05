401f34bdfcd4605829d178b78e7b6fb81af794dc
AHpx <ahpx@yandex.com>
2022-08-05 13:28:28 +0800

    Removed redundant processor

--- Manganese/Process/ProcessManipulator.cs
+++ Manganese/Process/ProcessManipulator.cs
@@ -53,7 +53,6 @@ public static class ProcessManipulator
             // hack because of this: https://github.com/dotnet/corefx/issues/10361
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                url = url.Replace("&", "^&");
                 Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
