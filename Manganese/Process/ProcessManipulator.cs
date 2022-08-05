ab1a4046bd3dbadbcdd9b1a5bec081d55fa509d3
AHpx <ahpx@yandex.com>
2022-08-05 13:06:03 +0800

    Added open url method

--- Manganese/Process/ProcessManipulator.cs
+++ Manganese/Process/ProcessManipulator.cs
@@ -1,4 +1,6 @@
-﻿namespace Manganese.Process;
+﻿using System.Runtime.InteropServices;
+
+namespace Manganese.Process;
 
 using System.Diagnostics;
 
@@ -25,5 +27,47 @@ public static class ProcessManipulator
     public static async Task<string?> ReadOutputLineAsync(this Process process)
     {
         return await process.StandardOutput.ReadLineAsync();
+    }
+
+    /// <summary>
+    /// Open some url in default browser
+    /// </summary>
+    /// <param name="uri"></param>
+    public static void OpenUrl(this Uri uri)
+    {
+        uri.ToString().OpenUrl();
+    }
+    
+    /// <summary>
+    /// Open some url in default browser
+    /// </summary>
+    /// <param name="uri"></param>
+    public static void OpenUrl(this string url)
+    {
+        try
+        {
+            Process.Start(url);
+        }
+        catch
+        {
+            // hack because of this: https://github.com/dotnet/corefx/issues/10361
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+            else
+            {
+                throw;
+            }
+        }
     }
 }
